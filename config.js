dc5af9e0121a39db5ceca0e91429f4817605982c
ThanhTu497 <106809303+ThanhTu497@users.noreply.github.com>
2022-06-03 16:48:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Người Yêu Thương Vô Bờ Bến",
+    introTitle: 'Lê Thị Lý',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
